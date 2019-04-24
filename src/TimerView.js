8a3813cfcc05b2a913e88627834b68fb1c059088
Tim <9349164+T1mL3arn@users.noreply.github.com>
2019-04-24 17:40:00 +1000

    make TimerView as functional component

--- src/TimerView.js
+++ src/TimerView.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { secondsToString } from './utils';
 
-class TimerView extends Component {
-  render() {
-    const { timerLabel, secondsLeft } = this.props
-    let timeLeft = secondsToString(secondsLeft)
+function TimerView(props) {
 
-    return (
-        <div>
-          <h2>{ timerLabel }</h2>
-          <p>time left</p>
-          <div>{ timeLeft }</div>
-        </div>
-    )
-  }
+  const { timerLabel, secondsLeft } = props
+  let timeLeft = secondsToString(secondsLeft)
+
+  return (
+    <div>
+      <h2>{ timerLabel }</h2>
+      <p>time left</p>
+      <div>{ timeLeft }</div>
+    </div>
+  )
 }
 
 export default TimerView;
